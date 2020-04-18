2d5e3aeeb3adf8e22de402aab5bd765e355f3183
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2020-04-18 20:59:11 +0300

    Update l0.py

--- Lectures/l0.py
+++ Lectures/l0.py
@@ -1,6 +1,7 @@
 from Bio.Seq import Seq
 
-seq=Seq("AGCTGATGCAGTCCAGT")#creating sequence
+seq=Seq("AGCTGATGCAGTCCAGT")#create sequence
 print(seq)
 print(seq.complement())#complement sequence of the sequence
 print(seq.reverse_complement())#reverse complement sequence of the sequence
+print(str(seq))#convert sequence into string
