13658b54bbd3f0ba3f51ed6fa7e6ef9ea0d4c964
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2020-04-18 21:43:26 +0300

    Update l2.py

--- Lectures/l2.py
+++ Lectures/l2.py
@@ -1,4 +1,3 @@
-
 from Bio.Alphabet import IUPAC
 from Bio import Seq
 from Bio.SeqUtils import GC
@@ -7,10 +6,3 @@ print(GC(my_dna))#Percentage of Guanine and Cytosine in sequance
 my_rna=Seq.Seq("AUGGUUAGUUAA",IUPAC.unambiguous_rna)
 
 my_protein=Seq.Seq("MVS*",IUPAC.protein)
-
-#Tyrosine synthesis
-dna=Seq.Seq("ATGTATTAA",IUPAC.unambiguous_dna)
-rna=dna.transcribe()
-protein=rna.translate()
-#removing methionine 
-protein=protein[1:]#We get an amino-acid. 
